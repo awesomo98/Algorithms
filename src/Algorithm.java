126449630392f3d2b179d832dd1fc554bb34ec3b
Bilal Reza <rezasb@s.dcsdk12.org>
2016-01-22 09:55:23 -0700

    Binary search

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -7,43 +7,43 @@ public class Algorithm {
 
 	 }
 	
-	// public static int max (int[] a) {
-	// 	int max = a[0];
-	// 	for (int n : a) {
-	// 		if (n > max) {
-	// 			max = n;
-	// 		}
-	// 	}
-	// 	return max;
-	// }
+	public static int max (int[] a) {
+		int max = a[0];
+		for (int n : a) {
+			if (n > max) {
+				max = n;
+			}
+		}
+		return max;
+	}
 	
-	// public static int[] sort (int[] data) {
-	// 	int[] sorted = new int[data.length];
+	public static int[] sort (int[] data) {
+		int[] sorted = new int[data.length];
 		
-	// 	int max = 0;
-	// 	int index = 0;
+		int max = 0;
+		int index = 0;
 		
-	// 	for(int n = 0; n < data.length; n++) {
-	// 		if (data[n] > max) {
-	// 			max = data[n];
-	// 			index = n;
-	// 		}
-	// 	}
-	// 	sorted[index] = max;
-	// 	data[index] = 0;
-	// 	return sorted;
+		for(int n = 0; n < data.length; n++) {
+			if (data[n] > max) {
+				max = data[n];
+				index = n;
+			}
+		}
+		sorted[index] = max;
+		data[index] = 0;
+		return sorted;
 		
 		
-	// }
+	}
 	
-	// public static int[] bubbleSort (int[] data) {
-	// 	for (int n = 0; n < data.length; n++) {
-	// 		for (int i = 0; i < data.length - 1; i++) {
-	// 			if (a[i] > )
-	// 		}
-	// 	}
+	public static int[] bubbleSort (int[] data) {
+		for (int n = 0; n < data.length; n++) {
+			for (int i = 0; i < data.length - 1; i++) {
+				if (a[i] > )
+			}
+		}
 		
-	// }
+	}
 	
 	public static void search(String[] arr, String targetValue) {
 		for (String s : arr) {
@@ -53,5 +53,37 @@ public class Algorithm {
 				System.out.println("This string does not exist");
 			}
 		}
+	}
+	
+	public static void binarySearch(int[ ] array, int lowerbound, int upperbound, int key)
+	   {
+	       int position;
+	       int comparisonCount = 1;   
+
+	  
+	     position = ( lowerbound + upperbound) / 2;
+
+	     while((array[position] != key) && (lowerbound <= upperbound))
+	     {
+	         comparisonCount++;
+	         if (array[position] > key)
+	              upperbound = position - 1;   
+	         }                                                             
+	              else                                                   
+	        {                                                        
+	              lowerbound = position + 1;
+	        }
+	       position = (lowerbound + upperbound) / 2;
+	     }
+	     if (lowerbound <= upperbound)
+	     {
+	           System.out.println("The number is in " + position);
+	           
+	     }
+	     else
+	          System.out.println("Sorry, the number is not in this array.  The binary search made "
+	                 +comparisonCount  + " comparisons.");
+	  }
+
 	}
 }
